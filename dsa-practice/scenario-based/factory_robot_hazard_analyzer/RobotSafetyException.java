b38e91c7a3f8a44728d93bac820ad5fef9b55a2b
Rishabh Shukla <rishabhshukla01032004@gmail.com>
2026-02-03 16:44:45 +0530

    [rishabh]  Add . scenario based

---
+++ dsa-practice/scenario-based/factory_robot_hazard_analyzer/RobotSafetyException.java
@@ -0,0 +1,8 @@
+package factory_robot_hazard_analyzer;
+
+public class RobotSafetyException extends Exception {
+
+    public RobotSafetyException(String message) {
+        super(message);
+    }
+}
